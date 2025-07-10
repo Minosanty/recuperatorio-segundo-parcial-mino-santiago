4401014e9a19fbf10a1ddb4897dbc1454002349b
Minosanty <minosantiago17@gmail.com>
2025-07-10 14:56:22 -0300

    se desarrollaron el archivo language. model en el cual se creo (ID,Name,Paradigm,Release_year)

---
+++ src/models/language.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+const Language = sequelize.define(
+    "language",{
+             id:{
+            type: DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+         },
+            name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        
+         },
+            
+          paradigm:{
+            type: DataTypes.STRING,
+            allowNull: false,
+
+         }, release_year:{
+            type: DataTypes.STRING,
+            allowNull: false,   
+        }
+        }
+)   
+export default Language
